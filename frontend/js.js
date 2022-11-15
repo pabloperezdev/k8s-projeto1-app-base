542d59c12e47ae1dbc859c6b11652675c841d831
pabloperezdev <pabloperez1987@gmail.com>
2022-11-14 23:06:18 -0300

    Update js.js

--- frontend/js.js
+++ frontend/js.js
@@ -5,7 +5,7 @@ $("#button-blue").on("click", function() {
     var txt_comentario = $("#comment").val();
 
     $.ajax({
-        url: "<NaoPegouIP> https://<IP>",
+        url: "",
         
         type: "post",
         data: {nome: txt_nome, comentario: txt_comentario, email: txt_email},
