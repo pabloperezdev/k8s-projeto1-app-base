609e299ebdb66cad217ebe2fcd384451cc933d8b
pabloperezdev <pabloperez1987@gmail.com>
2022-11-14 23:05:00 -0300

    Add files via upload

---
+++ script.bat
@@ -0,0 +1,18 @@
+echo "Criando as imagens ..."
+
+docker build-t pabloperezdev/projeto-backend:1.0 backend/.
+docker build-t pabloperezdev/projeto-database:1.0 database/.
+
+echo "Realizando push das imagens..."
+
+docker push pabloperezdev/projeto-backend:1.0
+docker push pabloperezdev/projeto-database:1.0
+
+echo "Criando servicos no cluster kubernets ..."
+
+kubectl apply -f ./services.yml
+
+echo "Criando os deployments ....."
+
+kubectl apply -f ./deployment.yml
+
